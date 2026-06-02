ac92e65887bd5ea5d866523cc3a45a4ce89ba09a
Esteban Dev <dominguezesteban1020@gmail.com>
2026-06-01 21:31:39 -0500

    Componente de catálogo creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos';
 import { LoginComponent } from './components/login/login';
-import { CatalogoProductosComponent } from './components/catalogo-productos/catalogo-productos';
+
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'catalogo', component: CatalogoProductosComponent },
     { path: 'listar-productos', component: ListarProductosComponent },
     { path: 'crear-producto', component: CrearProductoComponent },
     { path: 'editar-producto/:id', component: CrearProductoComponent },
