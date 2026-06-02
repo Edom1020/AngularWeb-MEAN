2f17920ca200c39eb4470bc7bc5d152304fc55c9
Esteban Dev <dominguezesteban1020@gmail.com>
2026-06-01 21:45:53 -0500

    Rutas ajustadas de catalogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos';
 import { LoginComponent } from './components/login/login';
+import { CatalogoProductosComponent } from './components/catalogo-productos/catalogo-productos';
+
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'catalogo', component: CatalogoProductosComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'listar-productos', component: ListarProductosComponent },
     { path: 'crear-producto', component: CrearProductoComponent },
